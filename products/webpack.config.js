f1d1e525c5d4c1efe012aa0346b097f46a713811
TalissonMelo <talissonmelorodrigues@gmail.com>
2026-02-03 20:15:54 -0300

    Integrating microfront-ends into the main project

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = withModuleFederationPlugin({
   name: 'products',
 
   exposes: {
-    './Component': './src\app\app.ts',
+    './Component': './src/app/app.ts',
   },
 
   shared: {
