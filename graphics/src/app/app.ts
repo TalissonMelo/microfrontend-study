6d37e99b1eb24cf40996f5cb3ac7138f67c90f78
TalissonMelo <talissonmelorodrigues@gmail.com>
2026-02-05 20:37:44 -0300

    Style to products

--- graphics/src/app/app.ts
+++ graphics/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,40 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('graphics');
+export class App implements OnInit {
+  
+  ngOnInit(): void {
+    const data = [
+      { category: 'Eletronicos', price: 10 },
+      { category: 'Roupas', price: 15 },
+      { category: 'Decoração', price: 5 },
+      { category: 'Ferramentas', price: 20 },
+      { category: 'Acessórios', price: 17 }
+    ];
+
+    const labels = data.map(v => v.category);
+    const prices = data.map(v => v.price);
+
+    new Chart('barChart', {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'categorys Mais Vendidas',
+            data: prices,
+            backgroundColor: 'rgba(31, 82, 146, 0.6)'
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales:  {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
