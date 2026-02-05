ab1ffd7f7f7306fb57dc8c53540fee15928e73bc
TalissonMelo <talissonmelorodrigues@gmail.com>
2026-02-05 18:50:49 -0300

    Creating the project's main page

--- products/src/app/app.ts
+++ products/src/app/app.ts
@@ -1,4 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Product {
+  name: string;
+  price: number;
+  image: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,5 +13,31 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('products');
+  products: Product[] = [
+    {
+      name: 'Camiseta Angular',
+      price: 79.90,
+      image: 'https://picsum.photos/id/1/200/150'
+    },
+    {
+      name: 'Caneca Programador',
+      price: 49.90,
+      image: 'https://picsum.photos/id/10/200/150'
+    },
+    {
+      name: 'Mouse Gamer RGB',
+      price: 129.90,
+      image: 'https://picsum.photos/id/25/200/150'
+    },
+    {
+      name: 'Teclado Mecânico',
+      price: 299.00,
+      image: 'https://picsum.photos/id/30/200/150'
+    },
+    {
+      name: 'Headset Bluetooth',
+      price: 199.90,
+      image: 'https://picsum.photos/id/40/200/150'
+    }
+  ];
 }
